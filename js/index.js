2d272c6a8f6dc9dbabbbcfd9d05d6818149788c1
sarah-nolan <snolan202@gmail.com>
2016-12-11 14:18:55 -0500

    HW4 Initial Commit
    Reveal more info when user presses "Read More" and hide when user
    presses "Read Less"
    
    Reveal more info when user presses "Learn More"

---
+++ js/index.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function() {
+  $('.readmore').click(function(){
+    $('#show-this-on-click').slideDown();
+    $('#show-this-on-click').show();
+    $('.readless').show();
+    $('.readmore').hide();
+    event.preventDefault();
+  });
+});
+
+$(document).ready(function() {
+  $('.readless').click(function(){
+    $('.readless').hide();
+    $('.readmore').show();
+    $("#show-this-on-click").hide(500);
+    $("#show-this-on-click").slideUp();
+  	event.preventDefault();
+  });
+});
+
+$(document).ready(function() {
+  $('#learnmorebutton').click(function(){
+  	$('#learnmoretext').slideDown();
+  	$('#learnmoretext').show();
+    $('#learnmorebutton').hide();
+    $('#lessbutton').show();
+  	event.preventDefault();
+  });
+});
